1bfdc9ce499ef647bdd0183122a721dac10d98b9
devPinheiro <samuelpinheiro41@gmail.com>
2019-11-04 17:29:15 +0100

    update hero

---
+++ components/Hero/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="xl:flex lg:flex block mb-4">
+                    <div class="block xl:flex-1 lg:flex-1  text-dark  ">
+                        <div class="xl:pt-24 xl:mt-18 xl:pr-32  lg:pt-40 lg:mt-18 lg:pr-32 ">
+                          <h6 class="font-Montserrat text-base sm:text-lg md:text-xl pt-4 pb-12 text-dark-400 ">
+                            Hi, I'm Samuel
+                            </h6>
+                            <h1 class="font-Montserrat leading-tight sm:text-3xl md:text-4xl lg-text-5xl xl:text-5xl text-2xl">
+                            Developer & Designer
+                            </h1>
+                            
+                            <div class=" m-auto text-center xl:text-left lg:text-left mb-8 ">
+                               
+                            </div>
+                        </div>
+                    </div>
+                    <div class=" block xl:flex-1 lg:flex-1 pt-8 m-auto  ">
+                       Image goes here
+                    </div>
+                </div>
+</template>
+
+<script>
+  export default {
+  
+  }
+</script>
+
+<style lang="css" >
+
+
+</style>
