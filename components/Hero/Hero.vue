0e25ed8a58d739fdc1502cdc44438a6c48a9f9b7
devPinheiro <samuelpinheiro41@gmail.com>
2019-11-04 17:29:15 +0100

    update hero

--- components/Hero/Hero.vue
+++ components/Hero/Hero.vue
@@ -1,20 +1,20 @@
 <template>
-  <div className="flex content-between flex-wrap ">
-                    <div className="w-1/2  text-dark  ">
-                        <div className="xl:pt-24 xl:mt-18 xl:pr-32  lg:pt-40 lg:mt-18 lg:pr-32 ">
-                          <h6 className="font-Montserrat text-base sm:text-lg md:text-xl pt-4 pb-12 text-dark-400 ">
+  <div class="xl:flex lg:flex block mb-4">
+                    <div class="block xl:flex-1 lg:flex-1  text-dark  ">
+                        <div class="xl:pt-24 xl:mt-18 xl:pr-32  lg:pt-40 lg:mt-18 lg:pr-32 ">
+                          <h6 class="font-Montserrat text-base sm:text-lg md:text-xl pt-4 pb-12 text-dark-400 ">
                             Hi, I'm Samuel
                             </h6>
-                            <h1 className="font-raleway leading-tight sm:text-3xl md:text-4xl lg-text-5xl xl:text-5xl text-2xl">
+                            <h1 class="font-Montserrat leading-tight sm:text-3xl md:text-4xl lg-text-5xl xl:text-5xl text-2xl">
                             Developer & Designer
                             </h1>
                             
-                            <div className=" m-auto text-center xl:text-left lg:text-left mb-8 ">
+                            <div class=" m-auto text-center xl:text-left lg:text-left mb-8 ">
                                
                             </div>
                         </div>
                     </div>
-                    <div className="w-1/2  ">
+                    <div class=" block xl:flex-1 lg:flex-1 pt-8 m-auto  ">
                        Image goes here
                     </div>
                 </div>
