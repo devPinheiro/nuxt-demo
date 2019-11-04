1338d27353522a29ff5f5823f947ad5a7d820ab2
devPinheiro <samuelpinheiro41@gmail.com>
2019-11-04 18:11:05 +0100

    tailwindcss set up

--- components/Hero/Hero.vue
+++ components/Hero/Hero.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="xl:flex lg:flex block mb-4">
-                    <div class="block xl:flex-1 lg:flex-1  text-dark  ">
-                        <div class="xl:pt-24 xl:mt-18 xl:pr-32  lg:pt-40 lg:mt-18 lg:pr-32 ">
-                          <h6 class="font-Montserrat text-base sm:text-lg md:text-xl pt-4 pb-12 text-dark-400 ">
-                            Hi, I'm Samuel
-                            </h6>
-                            <h1 class="font-Montserrat leading-tight sm:text-3xl md:text-4xl lg-text-5xl xl:text-5xl text-2xl">
+  <div class="w-full max-w-screen-xl relative mx-auto px-6  sm:pr-20 sm:pl-20 xl:flex lg:flex block mb-4 bg-gray-10">
+                    <div class="block md:flex xl:flex lg:flex  text-dark  ">
+                        <div class="xl:pt-40 xl:mt-18 xl:pr-32 pt-40 lg:pt-40 lg:mt-18 lg:pr-32 ">
+                          <h1 class="font-Montserrat font-light text-base sm:text-lg md:text-xl lg:text-5xl pt-4 pb-2 text-dark-400 ">
+                            Hi, I'm <span class="font-normal">Samuel</span>
+                            </h1>
+                            <h1 class="font-Montserrat font-bold leading-tight sm:text-3xl md:text-4xl lg:text-6xl xl:text-6xl text-xl">
                             Developer & Designer
                             </h1>
                             
-                            <div class=" m-auto text-center xl:text-left lg:text-left mb-8 ">
-                               
-                            </div>
+                           
                         </div>
                     </div>
-                    <div class=" block xl:flex-1 lg:flex-1 pt-8 m-auto  ">
+                    <div class="block md:flex xl:flex lg:flex pt-8 m-auto  ">
                        Image goes here
                     </div>
                 </div>
--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -1,11 +1,18 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const { defaultTheme, backgroundColor } = require('tailwindcss/defaultTheme')
 
 module.exports = {
   prefix: '',
   important: false,
   separator: ':',
   theme: {
+
     extend: {
+      colors: {
+        gray: {
+            ...colors.gray,
+            '10': '#f5f5f5',  
+        }
+      },
       fontFamily: {
         sans: [
           'Montserrat',
