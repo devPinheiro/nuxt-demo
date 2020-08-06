6ff658c9949d03719683286fbbedbf4a667039ae
Pinheiro <user@Users-MacBook-Pro.local>
2020-08-06 01:37:46 +0100

    WIP

--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,7 @@
 <template>
     <div>
         <Hero />
-        <div class="w-full max-w-screen-xl pt-32  lg:pt-40 mx-auto px-6 sm:pr-20 sm:pl-20  lg:pr-32 lg:pl-32 flex flex-row flex-col mb-4 "> 
-          <div class="block text-center">
-            <h1 class="text-3xl font-thin">
-              I built some cool projects, take a peep!
-            </h1>
-            <theme-toggler></theme-toggler>
-          </div>
-          <div class="block lg:flex my-12">
-              <div class="block h-452 w-full lg:w-2/3 adtalk_banner mb-6 lg:mr-4">     
-                <Card class="flex ">   
-                         <div class="bg-gray-200  text-white font-Montserrat text-xs font-normal py-2 px-4 border rounded-full m-5 pr-6 pl-6"> UI Design</div>                    
-                          <div class="ml-auto m-5 rounded-full hover:shadow-xl hover:w-6">
-                             <svg width="35" class=" " height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M17.6667 34.3652C26.8714 34.3652 34.3333 26.8962 34.3333 17.6826C34.3333 8.46906 26.8714 1 17.6667 1C8.46192 1 1 8.46906 1 17.6826C1 26.8962 8.46192 34.3652 17.6667 34.3652Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M17.6667 24.3557L24.3333 17.6827L17.6667 11.0096" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M11 17.6826H24.3333" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              </svg>
-                          </div>                      
-                  
-                </Card>
-              </div>
-              <div class="block h-452 lg:w-1/3 adtalk_banner">     
-                 <Card class="flex ">   
-                         <div class="bg-transparent  text-white font-Montserrat text-xs font-normal hover:text-white py-2 px-4 border border-blue-500  rounded-full m-5 bg-white-800 pr-6 pl-6"> UI Design</div>                    
-                          <div class="ml-auto m-5">
-                             <svg width="35" class=" " height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M17.6667 34.3652C26.8714 34.3652 34.3333 26.8962 34.3333 17.6826C34.3333 8.46906 26.8714 1 17.6667 1C8.46192 1 1 8.46906 1 17.6826C1 26.8962 8.46192 34.3652 17.6667 34.3652Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M17.6667 24.3557L24.3333 17.6827L17.6667 11.0096" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M11 17.6826H24.3333" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              </svg>
-                          </div>                      
-                  
-                </Card>
-              </div>
-
-          </div>
-
-          <div class="block lg:flex my-12">
-
-               <div class="block h-452 lg:w-1/3 adtalk_banner mb-6 lg:mr-4">     
-                 <Card class="flex ">   
-                         <div class="bg-transparent  text-white font-Montserrat text-xs font-normal hover:text-white py-2 px-4 border border-blue-500  rounded-full m-5 bg-white-800 pr-6 pl-6"> UI Design</div>                    
-                          <div class="ml-auto m-5">
-                             <svg width="35" class=" " height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M17.6667 34.3652C26.8714 34.3652 34.3333 26.8962 34.3333 17.6826C34.3333 8.46906 26.8714 1 17.6667 1C8.46192 1 1 8.46906 1 17.6826C1 26.8962 8.46192 34.3652 17.6667 34.3652Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M17.6667 24.3557L24.3333 17.6827L17.6667 11.0096" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M11 17.6826H24.3333" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              </svg>
-                          </div>                      
-                  
-                </Card>
-              </div>
-
-              <div class="block h-452 w-full lg:w-2/3 cribsandspaces">     
-                <Card class="flex ">   
-                         <div class="bg-gray-200  text-white font-Montserrat text-xs font-normal py-2 px-4 border rounded-full m-5 pr-6 pl-6"> UI Design</div>                    
-                          <div class="ml-auto m-5 rounded-full hover:shadow-xl hover:w-6">
-                             <svg width="35" class=" " height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M17.6667 34.3652C26.8714 34.3652 34.3333 26.8962 34.3333 17.6826C34.3333 8.46906 26.8714 1 17.6667 1C8.46192 1 1 8.46906 1 17.6826C1 26.8962 8.46192 34.3652 17.6667 34.3652Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M17.6667 24.3557L24.3333 17.6827L17.6667 11.0096" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M11 17.6826H24.3333" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              </svg>
-                          </div>                      
-                  
-                </Card>
-              </div>
-             
-
-          </div>
-
-          <div class="block lg:flex my-12">
-           <Card class="lg:fllex h-452 w-full outlierz">
-             <div class="flex">
-                <div class="bg-gray-200  text-dark font-Montserrat text-xs font-normal py-2 px-4 border rounded-full m-5 pr-6 pl-6"> UI Design</div>                    
-                          <div class="ml-auto m-5 rounded-full hover:shadow-xl hover:w-6">
-                             <svg width="35" class=" " height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M17.6667 34.3652C26.8714 34.3652 34.3333 26.8962 34.3333 17.6826C34.3333 8.46906 26.8714 1 17.6667 1C8.46192 1 1 8.46906 1 17.6826C1 26.8962 8.46192 34.3652 17.6667 34.3652Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M17.6667 24.3557L24.3333 17.6827L17.6667 11.0096" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M11 17.6826H24.3333" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                              </svg>
-                          </div> 
-             </div>
-                  
-           </Card>
-          </div>
-        </div>
+       
 
         <!-- Blog Section -->
         <!-- <div class="w-full pt-12 lg:pt-40 px-6 m-auto lg:flex block mb-4"> 
@@ -118,7 +33,16 @@ export default {
     Card,
     Blog,
     ThemeToggler
-  }
+  },
+  // Watch for $route.query.page to call Component methods (asyncData, fetch, validate, layout, etc.)
+  watchQuery: ['page'],
+  // Key for <NuxtChild> (transitions)
+  key: to => to.fullPath,
+  // Called to know which transition to apply
+  transition (to, from) {
+    if (!from) { return 'slide-left' }
+    return 'slide-right' 
+  },
 }
 </script>
 
